973e318fa2bee4c9db65e5fdc427025ec2ba3fc5
kaviya <Kaviyak612004@gmail.com>
2024-08-20 15:15:02 -0800

    Task--2 Stock

---
+++ Stock.java
@@ -0,0 +1,39 @@
+package com.Codealpha.dto;
+
+public class Stock {
+
+	    private String name;
+	    private int quantity;
+	    private double price;
+
+	    public Stock(String name, int quantity, double price) {
+	        this.name = name;
+	        this.quantity = quantity;
+	        this.price = price;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getQuantity() {
+	        return quantity;
+	    }
+
+	    public double getPrice() {
+	        return price;
+	    }
+
+	    public void removeQuantity(int quantityToRemove) {
+	        if (quantityToRemove <= quantity) {
+	            quantity -= quantityToRemove;
+	        } else {
+	            System.out.println("Not enough quantity to remove");
+	        }
+	    }
+
+	    public String toString() {
+	        return name + ", quantity: " + quantity + ", price: \u20B9" + price;
+	    }
+	}
+
